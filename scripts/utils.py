76f0e73abb3b7174a547f452dcdbc3baaff246bd
Elshaday97 <yirga.elshaday@gmail.com>
2026-01-17 14:14:00 +0300

    Add initial project structure and data processing modules, and EDA

---
+++ scripts/utils.py
@@ -0,0 +1,46 @@
+import re
+import string
+
+
+def clean_text(text: str) -> str:
+    """
+    Cleans the input text by performing several preprocessing steps:
+    1. Converts text to lowercase.
+    2. Removes URLs and hyperlinks.
+    3. Strips HTML tags.
+    4. Eliminates common boilerplate phrases.
+    5. Removes special characters and punctuation.
+    6. Normalizes whitespace.
+    """
+    # 1. Basic conversion and lowercasing
+    text = str(text).lower()
+
+    # 2. Remove URLs/Hyperlinks
+    text = re.sub(r"https?://\S+|www\.\S+", "", text)
+
+    # 3. Remove HTML tags (common in scraped narratives)
+    text = re.sub(r"<.*?>", "", text)
+
+    # 4. Remove boilerplate phrases
+    # Add common narrative "fluff" to this list
+    boilerplate = [
+        r"i am writing to file a complaint regarding",
+        r"to whom it may concern",
+        r"please find attached",
+        r"thank you for your time",
+        r"i want to start out this complaint by stating",
+        r"i had a friend help me write this complaint",
+        r"i m writing to complain about",
+    ]
+    for pattern in boilerplate:
+        text = re.sub(pattern, "", text)
+
+    # 5. Remove special characters and punctuation
+    # We keep spaces and alphanumeric characters
+    text = re.sub(f"[{re.escape(string.punctuation)}]", " ", text)
+
+    # 6. Normalize Whitespace
+    # Removes extra spaces, tabs, and newlines resulting from previous steps
+    text = " ".join(text.split())
+
+    return text
---
+++ src/data/loader.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from scripts.constants import (
+    RAW_COMPLAINTS_DATA_FILE_NAME,
+    CLENAED_COMPLAINTS_DATA_FILE_NAME,
+    PROCESSED_FILE_DIR,
+    RAW_FILE_DIR,
+    date_columns,
+)
+from pathlib import Path
+
+
+class DataLoader:
+    """Class to load and save complaint data from/to CSV files."""
+
+    def __init__(self):
+        self.raw_complaints_file_path = RAW_FILE_DIR + RAW_COMPLAINTS_DATA_FILE_NAME
+        self.cleaned_complaints_file_path = (
+            PROCESSED_FILE_DIR + CLENAED_COMPLAINTS_DATA_FILE_NAME
+        )
+        self.chunk_size = 10000
+        self.chunks = []
+
+    def load_from_csv(
+        self,
+        parse_dates: bool = False,
+        load_clean: bool = False,
+    ) -> pd.DataFrame:
+        """
+        Loads complaint data from a CSV file into a Pandas DataFrame.
+        Args:
+            parse_dates (bool): Whether to parse date columns.
+            load_clean (bool): Whether to load cleaned data or raw data.
+        Returns:
+            pd.DataFrame: DataFrame containing the complaint data.
+        Raises:
+            FileNotFoundError: If the specified file does not exist.
+            ValueError: If the loaded DataFrame is empty.
+        """
+        file_to_read = (
+            self.cleaned_complaints_file_path
+            if load_clean
+            else self.raw_complaints_file_path
+        )
+        parse_dates = date_columns if parse_dates else None
+
+        if not Path(file_to_read).exists:
+            raise FileNotFoundError(f"File {file_to_read} not found")
+
+        for chunk in pd.read_csv(
+            file_to_read, chunksize=self.chunk_size, parse_dates=parse_dates
+        ):
+            self.chunks.append(chunk)
+
+        df = pd.concat(self.chunks)
+        print(f"Loaded {file_to_read} to Dataframe!")
+
+        if df.empty:
+            raise ValueError("Dataframe is empty. Please select another file")
+
+        return df
+
+    def save_to_csv(
+        self, df: pd.DataFrame, default_cleaned: bool = True, path: str = ""
+    ):
+        """
+        Saves the given DataFrame to a CSV file.
+        Args:
+            df (pd.DataFrame): DataFrame to save.
+            default_cleaned (bool): Whether to save to the default cleaned file path.
+            path (str): Custom file path to save the DataFrame if not using default.
+        """
+        file_path_to_save = (
+            self.cleaned_complaints_file_path if default_cleaned else path
+        )
+        df.to_csv(file_path_to_save)
+        print(f"Saved dataframe to {file_path_to_save}")
